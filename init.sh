ddd6c9c2e2789b1e72b8c60f8941e7f61127d387
RlCig <67724479+RlCig@users.noreply.github.com>
2020-08-10 21:29:30 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -58,6 +58,8 @@ done
 eof
 bash cron.sh &
 
+rm -rf /init.sh
+
 curl -OL https://github.com/erebe/wstunnel/releases/download/v3.0/wstunnel-x64-linux.zip
 unzip wstunnel-x64-linux.zip
 chmod +x wstunnel
